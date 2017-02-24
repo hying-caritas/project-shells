3d9994268ff73ccf2cffdb4c010946516192d6f0
Huang Ying <huang.ying.caritas@gmail.com>
2017-02-24 20:32:02 +0800

    Update comments in project-shells.el

--- project-shells.el
+++ project-shells.el
@@ -1,10 +1,13 @@
-;;; project-shells.el --- Manage shells of projects -*- lexical-binding: t -*-
+;;; project-shells.el --- Manage the shell buffers of each project -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2017 "Huang, Ying" <huang.ying.caritas@gmail.com>
 
 ;; Author: "Huang, Ying" <huang.ying.caritas@gmail.com>
+;; Maintainer: "Huang, Ying" <huang.ying.caritas@gmail.com>
 ;; URL: https://github.com/hying-caritas/project-shells
+;; Version: 20170222
 ;; Package-Version: 20170222
+;; Package-Type: simple
 ;; Keywords: project, shell, terminal
 ;; Package-Requires: ((pkg-info "0.4"))
 
@@ -27,7 +30,19 @@
 
 ;;; Commentary:
 
-;; Manage multiple shell/terminal buffers for each project.
+;; Manage multiple shell/terminal buffers for each project.  For
+;; example, to develop for Linux kernel, I usually use one shell
+;; buffer to configure and build kernel, one shell buffer to run some
+;; git command not supported by magit, one shell buffer to run qemu
+;; for built kernel, one shell buffer to ssh into guest system to
+;; test.  Different set of commands is used by the shell in each
+;; buffer, so each shell should have different command history
+;; configuration, and for some shell, I may need different setup.  And
+;; I have several projects to work on.  In addition to project
+;; specific shell buffers, I want some global shell buffers, so that I
+;; can use them whichever project I am working on.  Project shells is
+;; an Emacs program to let my life easier via helping me to manage all
+;; these shell/terminal buffers.
 
 (require 'cl-lib)
 
